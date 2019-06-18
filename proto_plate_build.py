82c2732a083505284927f77d308b90e811b7c50e
Thomas Antonacci <antonacci.t.j@thomas-mbp-2.wusm-wifi.wucon.wustl.edu>
2019-06-18 11:59:36 -0500

    Added pipeline file functionality

--- proto_plate_build.py
+++ proto_plate_build.py
@@ -74,12 +74,6 @@ def get_object(object_id, object_tag):
         obj = smart_sheet_client.Sheets.get_sheet(str(object_id))
     return obj
 
-def get_wo_dir_list():
-    """work in progress"""
-
-    wo_dir_list = glob.glob('[0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
-    return wo_dir_list
-
 """Read in Dilution Drop off and generate plate and tracking sheets as well as upload to/ update Smartsheet
 
 TODO:   Get DDO sheet and convert to csv ---------
@@ -113,6 +107,19 @@ for file in excel_files:
 
 csv_file = 'dilution_drop_off.csv'
 
+# Create file and read in different headings for pipelines
+with open('pipelines_file.csv', 'r+') as pipe_f:
+    wgs_pipe_list = []
+    exome_pipe_list = []
+
+    pipe_read = csv.DictReader(pipe_f,delimiter=',')
+    pipe_head = pipe_read.fieldnames
+    for line in pipe_read:
+        if line['Pipeline'] == 'e':
+            exome_pipe_list.append(line['Name'])
+        elif line['Pipeline'] == 'w':
+            wgs_pipe_list.append(line['Name'])
+
 #open and read in csv
 with open(csv_file,'r+') as rf, open('others.{}.csv'.format(mmddyy),'w') as otherf:
     next(rf)
@@ -138,10 +145,11 @@ with open(csv_file,'r+') as rf, open('others.{}.csv'.format(mmddyy),'w') as othe
     wgs_file_d = csv.DictWriter(wgsf, delimiter=',', fieldnames=outfile_header_list)
     wgs_woids = []
 
+
     for line in ddo_dict:
 
         if exome_count == 96:
-            exome_filename = 'Fragmentation_Plate_Exome_' + str(exome_plate_count) + '_' + '_'.join(exome_woids)
+            exome_filename = 'Fragmentation_Plate_Exome_' + str(exome_plate_count) + '_' + '_'.join(exome_woids) + '.csv'
             exomef.close()
             os.rename('exome_plates_temp_file',exome_filename)
             exomef = open('exome_plates_temp_file','w')
@@ -151,7 +159,7 @@ with open(csv_file,'r+') as rf, open('others.{}.csv'.format(mmddyy),'w') as othe
             exome_plate_count += 1
 
         if wgs_count == 96:
-            wgs_filename = 'Fragmentation_Plate_WGS_' + str(wgs_plate_count) + '_' + '_'.join(wgs_woids)
+            wgs_filename = 'Fragmentation_Plate_WGS_' + str(wgs_plate_count) + '_' + '_'.join(wgs_woids) + '.csv'
             wgsf.close()
             os.rename('wgs_plates_temp_file',wgs_filename)
             wgsf = open('wgs_plates_temp_file'.format(wgs_plate_count),'w')
@@ -160,10 +168,8 @@ with open(csv_file,'r+') as rf, open('others.{}.csv'.format(mmddyy),'w') as othe
             wgs_count = 0
             wgs_plate_count += 1
 
-        #need to add more categories and possibly methods in place of individual sections
-
         line_dict = {}
-        if 'Whole Genome Sequencing' in line['Outgoing Queue Work Order Pipeline'] or 'WGS' in line['Outgoing Queue Work Order Pipeline']:
+        if line['Outgoing Queue Work Order Pipeline'] in wgs_pipe_list:
 
             for head in outfile_header_list:
                 line_dict[head] = line[head]
@@ -171,7 +177,8 @@ with open(csv_file,'r+') as rf, open('others.{}.csv'.format(mmddyy),'w') as othe
             if line['Outgoing Queue Work Order'] not in wgs_woids:
                 wgs_woids.append(line['Outgoing Queue Work Order'])
             wgs_count += 1
-        elif 'Exome' in line['Outgoing Queue Work Order Pipeline']:
+
+        elif line['Outgoing Queue Work Order Pipeline'] in exome_pipe_list:
 
             for head in outfile_header_list:
                 line_dict[head] = line[head]
@@ -181,31 +188,49 @@ with open(csv_file,'r+') as rf, open('others.{}.csv'.format(mmddyy),'w') as othe
             exome_count += 1
         else:
 
-            pipeline_in = input('Is this WGS, Exome, or other.\nEnter w,e, or o: ')
+            pipeline_in = input('Is this WGS, Exome, or other: {}\nEnter w,e, or o: '.format(line['Outgoing Queue Work Order Pipeline']))
             if pipeline_in == 'w':
                 for head in outfile_header_list:
                     line_dict[head] = line[head]
                 wgs_file_d.writerow(line_dict)
-
                 wgs_count += 1
+                wgs_pipe_list.append(line['Outgoing Queue Work Order Pipeline'])
+
             elif pipeline_in == 'e':
                 for head in outfile_header_list:
                     line_dict[head] = line[head]
                 exome_file_d.writerow(line_dict)
                 exome_woids.append(line['Outgoing Queue Work Order'])
                 exome_count += 1
+                exome_pipe_list.append(line['Outgoing Queue Work Order Pipeline'])
+
             elif pipeline_in == 'o':
                 other_file_d.writerow(line)
             else:
-                input('Is this WGS, Exome, or other.\nPlease enter either w,e, or o: ')
-
+                input('Is this WGS, Exome, or other: {}\nPlease enter either w,e, or o: '.format(line['Outgoing Queue Work Order Pipeline']))
+exome_pipe_list
 if exome_count != 0:
     exomef.close()
-    exome_filename = 'Fragmentation_Plate_Exome_' + str(exome_plate_count) + '_' + '_'.join(exome_woids)
+    exome_filename = 'Fragmentation_Plate_Exome_' + str(exome_plate_count) + '_' + '_'.join(exome_woids) + '.csv'
     os.rename('exome_plates_temp_file',exome_filename)
 if wgs_count != 0:
     wgsf.close()
-    wgs_filename = 'Fragmentation_Plate_WGS_' + str(wgs_plate_count) + '_' +  '_'.join(wgs_woids)
+    wgs_filename = 'Fragmentation_Plate_WGS_' + str(wgs_plate_count) + '_' +  '_'.join(wgs_woids) + '.csv'
     os.rename('wgs_plates_temp_file',wgs_filename)
+
+with open('pipelines_file.csv', 'w') as pipe_f:
+    pipe_dict_write = csv.DictWriter(pipe_f,delimiter=',',fieldnames=pipe_head)
+    pipe_dict_write.writeheader()
+    pipe_dict = {'Name' : None,'Pipeline':None}
+
+    for name in wgs_pipe_list:
+        pipe_dict['Pipeline'] = 'w'
+        pipe_dict['Name'] = name
+        pipe_dict_write.writerow(pipe_dict)
+
+    for name in exome_pipe_list:
+        pipe_dict['Pipeline'] = 'e'
+        pipe_dict['Name'] = name
+        pipe_dict_write.writerow(pipe_dict)
 
 print('debug')
